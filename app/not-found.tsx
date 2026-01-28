5e7cd29b51d826bcd735190655dc721d40c652a0
Mudasar Farooq <mudassar.farooq@futuredevsolutions.com>
2026-01-28 12:45:21 +0500

    commit

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -5,13 +5,14 @@ import Link from "next/link";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 
+
 export default function NotFound() {
   return (
     <div className="flex w-full items-center justify-center min-h-screen bg-gray-100">
       <Card className="max-w-md text-center shadow-lg">
         <CardHeader>
           <CardTitle className="text-3xl font-bold text-red-600">
-           Page Not Found
+            Page Not Found
           </CardTitle>
         </CardHeader>
         <CardContent>
